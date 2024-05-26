faccd8e0fd25d1eaa4e188f899e67a30dc578756
zaeem2396 <zaeemansari87@gmail.com>
2024-05-27 00:12:00 +0530

    feat: swagger doc for user created

--- swagger.js
+++ swagger.js
@@ -2,15 +2,29 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Sentimental Analysis',
-      version: '1.0.0',
-      description: 'Sentimental Asalysis',
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Sentimental Analysis',
+            version: '1.0.0',
+            description: 'Sentimental Asalysis',
+        },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
     },
-  },
-  apis: ['./controllers/*.js'],
+    apis: ['./controllers/*.js'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
